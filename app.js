cdb0b8c46774ad7363bd08ed55e9adc59c7ed529
icorvus <business.wrona@gmail.com>
2022-01-19 17:22:24 +0100

    Added clear grid button which prompts user for new grid size

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 function createGrid(numberOfColumns) {
   const grid = document.getElementById('grid');
+  grid.textContent = ''
   const gridWidth =  grid.clientWidth;
 
   const cellWidth = gridWidth/numberOfColumns;
   for (let i = 0; i < numberOfColumns ** 2; i++){
     const cell = document.createElement('div');
-    cell.setAttribute('style', `box-sizing: border-box; width: ${cellWidth}px; height: ${cellWidth}px; border: 1px solid black`);
+    cell.setAttribute('style', `box-sizing: border-box; width: ${cellWidth}px;
+     height: ${cellWidth}px; border: 1px solid black`);
+    cell.addEventListener('mouseenter', colorToBlack);
     grid.appendChild(cell);
   }
 }
 
-createGrid(16);
+function clearGrid() {
+  gridSize = prompt("How many squares per side do you want?");
+  createGrid(gridSize);
+}
+
+const colorToBlack = (event) => {
+  event.target.style.backgroundColor = 'black';
+}
+
+clearButton = document.getElementById('clear-btn');
+clearButton.addEventListener('click', clearGrid)
+
+
+
+createGrid(32);
